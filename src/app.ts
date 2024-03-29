011e4edaeab60a1ab466afe7de935b90693c5d1a
Love <love.tr2014@gmail.com>
2024-03-29 21:44:01 +0100

    Task 2 completed: Authentication with dummyjson API enabled.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import json from 'body-parser';
 import fetch from 'node-fetch';
-import { Product, CartContent } from './types';
+import { Product, CartContent, User } from './types';
 
 const app = express();
 
@@ -30,7 +30,31 @@ app.get('/products', async (req: Request, res: Response) => {
 });
 
 app.post('/login', async (req: Request, res: Response) => {
-  res.send();
+  // Write a POST call that will take username and password from body and will authenticate against the endpoint https://dummyjson.com/auth/login
+  // If credentials are invalid throw the proper HTTP error.
+  // As response use type User (see type definition).
+  // Documentation is available at https://dummyjson.com/docs/auth
+  const { username, password } = req.body;
+  fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        username: username,
+        password: password,
+        expiresInMins: 30, // optional, defaults to 60
+      }),
+  })
+  // Check if login was successful
+    .then((response) => response.json() as Promise<User>)
+    .then((data) => {
+      // Check if login was successful
+      if (!data.token) {
+        // send unauthorized status response
+        res.status(401).send(data);
+      }
+      res.send(data);
+    })
+    .catch((error) => console.error('Error loggin in:', error));
 });
 
 app.post('/cart', async (req: Request, res: Response) => {
